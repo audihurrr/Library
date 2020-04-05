082cb38abe5fefc3d6acf873a61ab3924443943a
Miles <milesangelo@icloud.com>
2020-04-05 09:34:19 -0400

    Updates main branch

---
+++ Controllers/CatalogController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LibraryData;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library.Controllers
+{
+    public class CatalogController : Controller
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        private ILibraryAsset _assets;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="assets"></param>
+        public CatalogController(ILibraryAsset assets)
+        {
+            _assets = assets;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Index()
+        {
+            var assetModels = _assets.GetAll();
+       
+            return View();
+        }
+    }
+}
---
+++ Models/Catalog/AssetIndexListingModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Library.Models.Catalog
+{
+    public class AssetIndexListingModel
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public int Id { get; set; }
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        public string ImageUrl { get; set; }
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Title { get; set; }
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        public string AuthorOrDirector { get; set; }
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Type { get; set; }
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        public string DeweyCallNumer { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string NumberOfCopies { get; set; }
+    }
+}
